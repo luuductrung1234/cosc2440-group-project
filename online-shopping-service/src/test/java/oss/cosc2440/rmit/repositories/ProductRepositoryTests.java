a928a12039a8ca6bbb92b556d10bd822f977075f
Trung Luu <luuductrung1234@gmail.com>
2023-04-17 10:01:48 +0700

    clean up old tests

---
+++ online-shopping-service/src/test/java/oss/cosc2440/rmit/repositories/ProductRepositoryTests.java
@@ -0,0 +1,10 @@
+package oss.cosc2440.rmit.repositories;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductRepositoryTests {
+  @Test
+  public void listAllProductShouldSuccess() {
+    // TODO: write test
+  }
+}
