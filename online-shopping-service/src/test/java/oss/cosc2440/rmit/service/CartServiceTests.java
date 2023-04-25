6f1a3788cc62778ed22e5f70ff8e888a86b1825f
Trung Luu <luuductrung1234@gmail.com>
2023-04-25 20:46:37 +0700

    add tests

--- online-shopping-service/src/test/java/oss/cosc2440/rmit/service/CartServiceTests.java
+++ online-shopping-service/src/test/java/oss/cosc2440/rmit/service/CartServiceTests.java
@@ -1,11 +1,34 @@
 package oss.cosc2440.rmit.service;
 
 import org.junit.jupiter.api.Test;
+import oss.cosc2440.rmit.domain.ShoppingCart;
+import oss.cosc2440.rmit.seedwork.Constants;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CartServiceTests {
   @Test
-  public void getAllCartInDescendingWeightOrderTest() {
-    // TODO: write test
+  public void getAllCartInAscendingWeightOrderTest() {
+    // Setup
+    ClassLoader loader = ProductServiceTests.class.getClassLoader();
+    String pathToFile = Objects.requireNonNull(loader.getResource(Constants.CART_FILE_NAME)).getPath();
+    CartService cartService = new CartService(pathToFile);
+
+    // Action
+    List<ShoppingCart> carts = cartService.listAll();
+
+    // Assert
+    assertNotNull(carts);
+    assertTrue(carts.size() > 0);
+
+    assertTrue(carts.get(0).totalWeight() <= carts.get(1).totalWeight());
+    assertTrue(carts.get(1).totalWeight() <= carts.get(2).totalWeight());
+    assertTrue(carts.get(2).totalWeight() <= carts.get(3).totalWeight());
+    assertTrue(carts.get(3).totalWeight() <= carts.get(4).totalWeight());
   }
 
   @Test
--- online-shopping-service/src/test/java/oss/cosc2440/rmit/service/ProductServiceTests.java
+++ online-shopping-service/src/test/java/oss/cosc2440/rmit/service/ProductServiceTests.java
@@ -1,8 +1,32 @@
 package oss.cosc2440.rmit.service;
 
 import org.junit.jupiter.api.Test;
+import oss.cosc2440.rmit.domain.Product;
+import oss.cosc2440.rmit.model.SearchProductParameters;
+import oss.cosc2440.rmit.seedwork.Constants;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProductServiceTests {
+  @Test
+  public void listAllProductsShouldSuccess() {
+    // Setup
+    ClassLoader loader = ProductServiceTests.class.getClassLoader();
+    String pathToFile = Objects.requireNonNull(loader.getResource(Constants.PRODUCT_FILE_NAME)).getPath();
+    ProductService productService = new ProductService(pathToFile);
+
+    // Action
+    List<Product> products = productService.listAll(new SearchProductParameters());
+
+    // Assert
+    assertNotNull(products);
+    assertTrue(products.size() > 0);
+  }
+
   @Test
   public void createProductShouldSuccess() {
     // TODO: write test
